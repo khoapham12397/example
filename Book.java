babc1bd561c791d8776cc44c624a12e0e03c8ffc
Pham Minh Khoa <khoa.pham12397@hcmut.edu.vn>
2022-10-17 16:44:41 +0700

    add new line book

--- Book.java
+++ Book.java
@@ -3,6 +3,7 @@ public class Book {
     public Book(){
         this.name = "name_default";
         System.out.println("name : hello");
+        System.out.println("add new line");
     }
     public Book(String name) {
         this.name = name;
