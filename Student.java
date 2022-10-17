bd0f420e4d22446e359eaa26603130c92f0ec6d2
Pham Minh Khoa <khoa.pham12397@hcmut.edu.vn>
2022-10-17 11:59:50 +0700

    print name

--- Student.java
+++ Student.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Student {
     private Teacher myTeacher;
+
     public Student(Teacher tc){
         this.myTeacher = tc;
     }
@@ -10,6 +11,9 @@ public class Student {
     }
 }
 
+// that su la co nhieu van de dang xay ra trong day ???
+// dung vay do ma just do it and nhan xet the nay khi ma thang nay chuyen doi branch voi cai thang repo nay 
+// thi sao cung ko xac dinh ?
 
 
 
