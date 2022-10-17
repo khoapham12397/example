7dc1d1edd6f257cb7176ac218bba3a0e6b8fd304
Pham Minh Khoa <khoa.pham12397@hcmut.edu.vn>
2022-10-17 12:07:48 +0700

    cm

--- Book.java
+++ Book.java
@@ -2,6 +2,7 @@ public class Book {
     private String name;
     public Book(){
         this.name = "name_default";
+        System.out.println("name : "+ this.name);
     }
     public Book(String name) {
         this.name = name;
