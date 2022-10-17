5de124f4c7170c4a43f12bff36db041c62304a7f
Pham Minh Khoa <khoa.pham12397@hcmut.edu.vn>
2022-10-17 10:31:49 +0700

    add name student

--- Student.java
+++ Student.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
 public class Student {
     private Teacher myTeacher;
+    private String name; 
+    
     public Student(Teacher tc){
         this.myTeacher = tc;
+        this.name = "default";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Teacher getTeacher(){
